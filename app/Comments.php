5ef51695db69c6be2f4ff79c4932e26fe374b58e
Tim Joosten <Tjoosten3@gmail.com>
2017-06-12 08:40:31 +0200

    Set author relation and mass-assign comments

--- app/Comments.php
+++ app/Comments.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comments extends Model
 {
-    //
+    /**
+     * Mass-assign fields for the database table.
+     *
+     * @return array
+     */
+    protected $fillable = ['comment', 'author_id'];
+
+    public function author()
+    {
+        return $this->belongsTo(User::class, 'author_id');
+    }
 }
